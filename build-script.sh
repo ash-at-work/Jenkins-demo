8993f8e76a2ba44831f600edd89235453e82f0eb
ash-at-work <137593604+ash-at-work@users.noreply.github.com>
2023-12-24 00:29:22 +0600

    Update build-script.sh

--- build-script.sh
+++ build-script.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 podman build -t my-apache2 .
-if podman ps | grep my-running-app
+if
+podman ps | grep my-running-app
 then
 podman rm my-running-app -f
-podman run --name my-running-app -p 8080:80 my-apache2
+podman run -d --name my-running-app -p 8080:80 my-apache2
 else
-podman run --name my-running-app -p 8080:80 my-apache2
+podman run -d --name my-running-app -p 8080:80 my-apache2
 fi
